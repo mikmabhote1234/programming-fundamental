8ae89567f00d9cc5b78f7d3e501ac308f0626e04
mikmabhote1234 <mikma.bhote123@gmail.com>
2016-11-14 13:07:05 +0545

    Add files via upload

---
+++ ask.c
@@ -0,0 +1,17 @@
+/*
+student name:Mikma bhote
+subject :programming fundamental
+roll no:
+lab sheet no:
+program:write a C program to print name,ask from user
+date:14 november 2016
+*/ # include <stdio.h>
+# include <conio.h>
+int main (){
+char name[3];
+printf ("what is your name\n");
+scanf ("%s",&name);
+printf ( " My name is:%s",name);
+getch ();
+return 0;
+}
