7c48b5779c68678f739c5d1e422e0cdae77251da
mikmabhote1234 <mikma.bhote123@gmail.com>
2016-11-16 12:13:37 +0545

    Add files via upload

---
+++ mikma12.c
@@ -0,0 +1,24 @@
+/*
+student name : Mikma Bhote
+subject : Programming fundamental
+roll no : 8
+lab sheet no : 2
+program : write a c program to print sum of two numbers asked from users.
+date : november 16 2016
+*/
+# include <stdio.h>
+# include <conio.h>
+int main (){
+int a, b, subtraction;
+printf ("enter first number\n");
+scanf ("%d",&a);
+printf ("enter second number\n");
+scanf ("%d",&b);
+subtraction = a-b;
+printf ("The subtraction of two number is :%d",subtraction);
+getch();
+return 0;
+}
+
+
+
