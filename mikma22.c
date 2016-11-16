5d17bbb51408d88bc099c0077e03b0045320e293
mikmabhote1234 <mikma.bhote123@gmail.com>
2016-11-16 12:22:56 +0545

    Add files via upload

---
+++ mikma22.c
@@ -0,0 +1,23 @@
+/*
+student name : Mikma Bhote
+subject : Programming fundamental
+roll no : 8
+lab sheet no : 2
+program : write a c program to print division of two numbers asked from users.
+date : november 16 2016
+*/
+# include <stdio.h>
+# include <conio.h>
+int main (){
+int a, b, division;
+printf ("enter first number\n");
+scanf ("%d",&a);
+printf ("enter second number\n");
+scanf ("%d",&b);
+division= a/b;
+printf ("The division of two number is :%d",division);
+getch();
+return 0;
+}
+
+
