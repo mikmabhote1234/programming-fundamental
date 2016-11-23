408112af2148c3d2dae0245ffbb4e696af72f776
mikmabhote1234 <mikma.bhote123@gmail.com>
2016-11-23 10:37:27 +0545

    sum of three digits number

---
+++ SUMM.c
@@ -0,0 +1,23 @@
+/*
+student name : Mikma bhote
+subject name : Programming fundamental
+roll no :08
+lab sheet no:03
+program :write a c program to read the temperature in fahrenheat and convert it into centigrade
+date :23 Nov.2016
+*/
+#include<stdio.h>
+#include<conio.h>
+int main(){
+float a,b,c,sum;
+printf("Enter the first number:\n");
+scanf("%f",&a);
+printf("Enter the second number:\n");
+scanf("%f",&b);
+printf("Enter the third number:\n");
+scanf("%f",&c);
+sum=a+b+c;
+printf("The sum of three digits is:%f",sum);
+getch();
+return 0;
+}
