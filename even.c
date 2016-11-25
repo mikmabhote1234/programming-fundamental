573f6702cc52464010a54655d5d38df64d7114e5
mikmabhote1234 <mikma.bhote123@gmail.com>
2016-11-25 10:43:50 +0545

    to check the number is whether even or odd

---
+++ even.c
@@ -0,0 +1,26 @@
+/*
+Student name: Mikma bhote
+Subject: Computer fundamentals
+Roll no:23
+Program : Write a c program to print arithmetic operation.
+Lab Sheet No:03
+Date: November,2016,20
+*/
+#include<stdio.h>
+int main(){
+int n;
+printf("Enter the number is:\n");
+scanf("%d",&n);
+if(n%2==0)
+{
+    printf("is even number\n");
+
+}
+else
+{
+    printf("is odd number\n");
+}
+return 0;
+}
+
+
