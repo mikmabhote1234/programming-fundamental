385552a7ecf59ddade3c9317b59f912a99b39613
mikmabhote1234 <mikma.bhote123@gmail.com>
2016-11-28 12:47:47 +0545

    Add files via upload

---
+++ square root.c
@@ -0,0 +1,20 @@
+/*
+student name :Mikma bhote
+roll no : 08
+lab sheet no : 07
+program : write a c program to print a square root
+date :28 Nov 2016
+*/
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+int main()
+{
+int a,squareroot;
+printf("Enter the number:\n");
+scanf("%d",&a);
+squareroot=sqrt(a);
+printf("The square of the number is:%d",squareroot);
+getch();
+return 0;
+}
