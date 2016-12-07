a1e2f2c31a107f1953cc5a60141b043e59061107
mikmabhote1234 <mikma.bhote123@gmail.com>
2016-12-07 11:27:07 +0545

    To print the result using of IF

--- operator.c
+++ operator.c
@@ -1,29 +1,24 @@
-/*
-student name : Mikma bhote
-subject name : Programming Fundamental
-roll no : 08
-lab sheet no : 08
-program : write a c program to print the use assignment operator
-data : 30 Nov 2016
-*/
 #include<stdio.h>
 #include<conio.h>
-int main()
-{
-    int a,b;
-    printf("Enter the first number:\n");
-    scanf("%d",&a);
-    printf("Enter the second number:\n");
-    scanf("%d",&b);
-    a+=b;
-    printf("Using of a+=b is:%d\n",a);
-    a-=b;
-    printf("Using of a-=b is:%d\n",a);
-    a*=b;
-    printf("Using of a*=b is:%d\n",a);
-    a/=b;
-    printf("Using of a/=b is:%d\n",a);
-    getch ();
-    return 0;
+	int main()
+	{
+	    int a,b;
+	    printf("Enter the value of a:");
+	    scanf("%d",&a);
+	    printf("Enter the value of b:");
+	    scanf("%d",&b);
+	    a==b;
+	    printf("use of == is %d\n",a==b);
+	    a>b;
+	    printf("use of > is %d\n",a>b);
+	    a<b;
+	    printf("use of |= is %d\n",a<b);
+	    a>=b;
+	    printf("use of >= is %d\n",a>=b);
+	    a<=b;
+	    printf("use of <= is %d\n",a<=b);
 
-}
+	    getch();
+	    return 0;
+
+	}
