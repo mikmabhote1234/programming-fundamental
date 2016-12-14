15ea59eeea22fa25ff0d0f069edca80b9c64e356
mikmabhote1234 <mikma.bhote123@gmail.com>
2016-12-14 10:57:46 +0545

    Add files via upload

---
+++ ternary operator.c
@@ -0,0 +1,25 @@
+
+/*
+Author : Mikma bhote
+Subject : Programming Fundamentals
+Roll NO :
+Lab Sheet NO : 12
+Program : Write a C program to find largest among three numbers using ternary operator
+Date : December 9 , 2016
+*/
+
+#include<stdio.h>
+int main()
+{
+	float a,b,c;
+	printf("Enter your first number:\n");
+	scanf("%f",&a);
+	printf("Enter your second number:\n");
+	scanf("%f",&b);
+	printf("Enter your third number:\n");
+	scanf("%f",&c);
+	(a>=b && a>=c)?printf("The first number %f is the largest",a):
+	(b>=c && b>=a)?printf("The second number %f is the largest",b):
+	printf("The third number %f is the largest",c);
+	return 0;
+}
