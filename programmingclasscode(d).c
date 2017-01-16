f10c4fee7307e5a9da69b3543755f2208d8c3957
mikmabhote1234 <mikma.bhote123@gmail.com>
2017-01-16 11:39:50 +0545

    Add files via upload

---
+++ programmingclasscode(d).c
@@ -0,0 +1,25 @@
+/*
+student name:Mikma bhote
+subject name:Programming Fundamental
+roll no:23
+lab sheet no:19
+program: wap to display the patterns:
+          1111
+          2222              
+data: 16 Jan 2017
+*/
+#include<stdio.h>
+int main()
+{
+ int i,j;
+ for (i=1;i<=5;i++)
+ {
+	 for (j=1;j<=5;j++)
+	 {
+		printf("%d",i);
+	 }
+	 printf("\n");
+ }
+ return 0;
+}         
+
