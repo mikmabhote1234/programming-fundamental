3dc4c305cd3a262e38a4f98b8089e10a69b02f22
mikmabhote1234 <mikma.bhote123@gmail.com>
2017-01-18 13:26:05 +0545

    Add files via upload

---
+++ lab20(a).c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void cube();
+int main()
+{
+    int n,result;
+    printf("enter the number\n");
+    scanf("%d",&n);
+    result=n*n*n;
+    printf("the cube number is:%d",result);
+}
