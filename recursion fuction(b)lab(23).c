07e3458ba66fa1da7209ede7d53e3ccac0a8c7c8
mikmabhote1234 <mikma.bhote123@gmail.com>
2017-01-31 21:23:36 -0800

    Add files via upload

---
+++ recursion fuction(b)lab(23).c
@@ -0,0 +1,13 @@
+
+/*
+ Student name: Mikma bhote
+ Subject name:Programming Fundemental
+ roll no: 23
+ lab sheet no: 23
+ program: wap to find to 
+           0  1  1  2  3  4................up to 10th term.
+ Data:29 Jan 2017
+*/
+#include<stdio.h>
+int main()
+{
