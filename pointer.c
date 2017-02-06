772b1997ce6ef1dad902ef251f3893b0884fa99f
mikmabhote1234 <mikma.bhote123@gmail.com>
2017-02-06 11:19:45 +0545

    Add files via upload

---
+++ pointer.c
@@ -0,0 +1,19 @@
+/*
+student name:Mikma bhote
+subject name:Programming fundamental
+roll no:23
+lab sheet no:26
+program: WAP to use pointer
+date:6 Feb 2017
+*/
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int x=5;
+    int *p;
+    p=&x;
+    printf("The memory address of variable is:%d",p);
+    getch();
+    return 0;
+}
